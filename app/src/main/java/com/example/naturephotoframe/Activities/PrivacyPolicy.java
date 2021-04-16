aee89e1c3e6d901f46d5fde62ad5d846080bd49e
bilalmehmood26 <bilalmehmood628@gmail.com>
2021-04-16 05:46:35 -0700

    exit screen

--- app/src/main/java/com/example/naturephotoframe/Activities/PrivacyPolicy.java
+++ app/src/main/java/com/example/naturephotoframe/Activities/PrivacyPolicy.java
@@ -24,7 +24,7 @@ public class PrivacyPolicy extends AppCompatActivity {
         agreeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(PrivacyPolicy.this, MainActivity.class));
+                startActivity(new Intent(PrivacyPolicy.this, InAppPurchase.class));
                 finish();
             }
         });
